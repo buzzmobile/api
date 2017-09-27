e27be6d02b26c6e1ba44765330745be892e82da5
harry patel <harry@unomee.com>
2017-09-27 10:31:18 +0100

    removed chunking as this was only needed on AWS. Added better logging to consider the large file. Converted embedded json field strings into objects resulting in the removal of a feed field (Telcos extra json) as it is unlikely we'd use that and I'm unable to convert.

--- src/handlers/importDataFeed.js
+++ src/handlers/importDataFeed.js
@@ -1,6 +1,7 @@
+/*eslint-disable no-console*/
+
 import unzip from "unzipper";
 import axios from "axios";
-// import fs from "fs";
 import es from "event-stream";
 
 import config from "../shared/config/config";
@@ -8,48 +9,60 @@ import csvToJson from "../shared/csvToJson";
 import db from "../shared/db";
 
 async function importDataFeed(useSmallFile) {
-    const smallFileUrl = "https://www.dropbox.com/s/zt02avx43j3q0wz/datafeed_218297-20-rows.csv.zip?dl=0&raw=1";
+    const smallFileUrl = "https://www.dropbox.com/s/7th59xx1iigau82/datafeed_218297-17-rows.csv.zip?dl=0&raw=1";
     const dataFeedFileUrlToUse = useSmallFile === "true" ? smallFileUrl : config.get("dataFeedFileUrl");
-    console.log("Downloading the following url:", dataFeedFileUrlToUse); //eslint-disable-line no-console
+    console.log("Downloading the following url:", dataFeedFileUrlToUse); 
 
     let lineNr = 0;
     let header = "";
-    let chunk = "";
-    const chunkSize = 5;
+    let dealCsv = "";
 
     const response = await axios({ method: "get", url: dataFeedFileUrlToUse, responseType: "stream" });
     response.data
         .pipe(unzip.ParseOne())
-        // .pipe(fs.createWriteStream("./express.csv"))
         .pipe(es.split())
         .pipe(es.mapSync(line => {
             const isheaderRow = lineNr === 0 && line.includes("aw_deep_link");
             if (isheaderRow) {
                 header = line;
-                chunk = header;
+                dealCsv = header;
             } else {
-                if (!isheaderRow && lineNr % chunkSize === 0) {
-                    chunk += "\n" + line;
-                    saveChunk(chunk);
-                    chunk = header;
-                } else {
-                    chunk += "\n" + line;
-                }
+                dealCsv += "\n" + line;
+                saveDeal(dealCsv);
+                dealCsv = header;
             }
             lineNr += 1;
-            //TODO: this will miss the last set of rows
+            logProgress(lineNr);
         })
         .on("error", e => { throw new Error(e); })
-        .on("end", () => { return Promise.resolve(); })
-        );
-    return;
+        .on("end", () => { 
+            console.log("processing completed. Including the header and empty rows, the total number of rows processed was:", lineNr); 
+            return Promise.resolve(); 
+        })
+    );
 }
 
-async function saveChunk(csvChunk) {
-    const deals = await csvToJson(csvChunk.replace(/(")[?=^"]/g, "'"));
-    deals.map(deal => {
-        console.log(deal.aw_deep_link); //eslint-disable-line no-console
-        db.deals.update({ aw_deep_link: deal.aw_deep_link }, deal, { upsert: true });
-    });
+async function saveDeal(dealCsv) {
+    const deals = await csvToJson(dealCsv.replace(/(")[?=^"]/g, "'"));
+    const deal = deals[0];
+    if (deal) {
+        Object.keys(deal).map(k => {
+            if (k.includes("_json")) {
+                if (deal[k]){
+                    deal[k] = JSON.parse(deal[k].replace(/'/g, `\"`)); //eslint-disable-line quotes
+                }
+            }
+        });
+        return db.deals.update({ aw_deep_link: deal.aw_deep_link }, deal, { upsert: true });
+    }
+    return Promise.resolve();
 }
+
+const logProgress = lineNr => {
+    if (lineNr % 10000 === 0) {
+        console.log("processing line:", lineNr); 
+    }
+};
+
+
 export default importDataFeed;
