5f057531a05e8d8c4fa578ef1c5ee5597a3d2ed4
harry patel <harry@unomee.com>
2017-10-04 13:35:13 +0100

    rename filterable Query whilst adding another parameter.

--- src/api/routes/graphQlRoute.js
+++ src/api/routes/graphQlRoute.js
@@ -6,15 +6,24 @@ import db from "../../shared/db";
 
 const app = express.Router();
 
-async function find(page, qry={} ){
+async function find(page, qry = {}) {
     const pageSize = 10;
-    return db.deals.find(qry, { skip: pageSize * (page - 1), limit: pageSize });    
+    return db.deals.find(qry, { skip: pageSize * (page - 1), limit: pageSize });
 }
 
-const root = { 
-    allDeals: async ({ page = 1 }) => find(page),
-    allDealsByMerchantCategory: async ({ page = 1, merchantCategory }) => {
-        return find(page, { merchant_category: merchantCategory });
+const addFilter = (qry, filterValue, filterAttribute) => {
+    if (filterValue !== "Any") {
+        qry[filterAttribute] = filterValue;
+    }
+};
+
+const root = {
+    allDeals: async({ page = 1 }) => find(page),
+    allDealsFiltered: async({ page = 1, merchantCategory = "Any", operatingSystem = "Any" }) => {
+        const qry = {};
+        addFilter(qry, merchantCategory, "merchant_category");
+        addFilter(qry, operatingSystem, "Telcos_device_features_json.os");
+        return find(page, qry);
     }
 };
 
