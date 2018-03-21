476acc2a30f710072378e901c8e8896e32b9fe3b
harry patel <harry@unomee.com>
2018-03-21 17:34:08 +0000

    increase page size to 30

--- src/api/routes/graphQlRoute.js
+++ src/api/routes/graphQlRoute.js
@@ -12,7 +12,7 @@ const app = express.Router();
 
 async function find(page, qry = {}, sort = {}) {
     log({ qry, sort });
-    const pageSize = 21;
+    const pageSize = 30;
     const dbDeals = await db.deals.find(qry, { skip: pageSize * (page - 1), limit: pageSize, sort });
     return addExternalId(dbDeals);
 }
@@ -26,9 +26,20 @@ async function addExternalId(dbDeals) {
     return dbDeals.map(d => Object.assign({ id: hashids.encodeHex(d._id) }, d));
 }
 
-const log = s => console.log(util.inspect(s, { showHidden: true, depth: null })); //eslint-disable-line no-console
+const log = s => console.log(util.inspect(s, { showHidden: false, depth: null })); //eslint-disable-line no-console
 
-const addFilter = (qry, filterQry) => Object.assign(qry, filterQry);
+const addFilter = (qry, filterQry) => {
+    if (filterQry && filterQry.$or) {
+        return combineOrFilters(qry, filterQry);
+    }
+    return Object.assign(qry, filterQry);
+};
+
+const combineOrFilters = (qry, filterQry) => {
+    qry.$and = qry.$and || [];
+    qry.$and.push(filterQry);
+    return qry;
+};
 
 const root = {
     allDeals: async({ page = 1 }) => find(page),
